40e5acfadd28f3e148c24f2cf953a35bcecd9e25
Kevin Ernest Long <kevinelong@gmail.com>
2025-06-10 15:58:29 -0700

    begin insert

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,8 @@
 package org.example;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 //CONTROLLER
 public class Main {
     public static void main(String[] args) {
@@ -10,5 +13,23 @@ public class Main {
         ui.printList(data.getCondiments(new String[]{"ProductName"}), "ProductName");
 
         data.close();
+
+
+        //input
+        String[] fields = {"firstName", "lastName", "notes"};
+        String[] values = {"Kevin", "Long", "Geek at large."};
+        String tableName = "Employees";
+
+        //get insert sql?
+//        "INSERT INTO table_name (column1, column2, column3, ...)\n" +
+//                "VALUES (value1, value2, value3, ...);";
+        //delimited by commas
+        String df = Arrays.stream(fields).map(s -> String.format("`%s`",s)).collect(Collectors.joining(","));
+        String dv = Arrays.stream(values).map(s -> String.format("'%s'",s)).collect(Collectors.joining(","));
+
+        String sqlInsert = String.format("INSERT INTO %s (%s) VALUES (%s);", tableName, df, dv);
+        System.out.println(sqlInsert);
+
+        //TODO RUN AND GET ID
     }
 }
